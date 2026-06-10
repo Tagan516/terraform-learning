554c75dcf6655ce918f3261075cbdb0202cb92ce
Tagan Williamson <93137070+Tagan516@users.noreply.github.com>
2026-06-10 16:23:00 -0400

    Add output values for EC2 instance, vpc, and subnet

---
+++ terraform-getting-started-5/ExerciseFiles/base_web_app/outputs.tf
@@ -0,0 +1,14 @@
+output "aws_instance_public_dns" {
+    value = aws_instance.nginx1.public_dns
+    description = "Public DNS hostname of the EC2 instance"
+}
+
+output "vpc_id" {
+    value = aws_vpc.app.id
+    description = "ID of the VPC"
+}
+
+output "subnet_id" {
+    value = aws_subnet.public_subnet1.id
+    description = "ID of the public subnet"
+}
